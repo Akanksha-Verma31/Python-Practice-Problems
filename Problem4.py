5b6d678b323a34d7ddb11fcf250a529ea932ce03
Akanksha Verma <22906.akanksha@gmail.com>
2021-08-28 18:46:05 +0530

    Create Problem4.py

---
+++ Problem4.py
@@ -0,0 +1,10 @@
+# The circumference of a circle is 2πr where r is the radius of the circle. 
+# Write a Python statement that calculates and prints the circumference in inches of a circle whose radius is 8 inches. 
+# Assume that the constant π=3.14. 
+
+radius = 8
+pi = 3.14
+
+circumference = 2*pi*radius
+
+print("Circumference of the circle with radius {} is " .format(radius) , circumference,"inches.")
