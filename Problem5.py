f0bb1c2cb72e440aba7f69ccd5ca038a5eee4544
Akanksha Verma <22906.akanksha@gmail.com>
2021-08-28 18:50:50 +0530

    Create Problem5.py

---
+++ Problem5.py
@@ -0,0 +1,9 @@
+# Write a Python expression that combines the string "Joe Warren is 52 years old." from the string "Joe Warren" and the number 52
+# and then prints the result (Hint: Use the function str to convert the number into a string.) 
+
+str1 = "Joe Warren"
+str2 = "52"
+
+print(str1+str2)
+print("Joe Warren "+"52")
+print("Joe Warren "+"is "+"52")
