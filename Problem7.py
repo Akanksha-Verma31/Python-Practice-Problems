ff97e6f67ee8f7e6e7a4cefad2176b3e8d75f1d0
Akanksha Verma <22906.akanksha@gmail.com>
2021-09-03 11:48:21 +0530

    Create Problem7.py

---
+++ Problem7.py
@@ -0,0 +1,4 @@
+# Take a number from user and print whether that number is divisible by 10 or not.
+
+num = int(input("Enter a number : "))
+print("Divisible") if num%10 ==0 else print("Not Divisible")
