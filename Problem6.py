ba2cf36aac83cfa7f4240d5d0d9eb2a3cbdf6760
Akanksha Verma <22906.akanksha@gmail.com>
2021-09-03 11:43:20 +0530

    Create Problem6.py

---
+++ Problem6.py
@@ -0,0 +1,8 @@
+# print hello if user enters a number which is multiple of 5, otherwise print bye.
+
+x = int(input("Enter a Number: "))
+if x%5 == 0:
+    print("Hello")
+else:
+    print("Bye")
+   
