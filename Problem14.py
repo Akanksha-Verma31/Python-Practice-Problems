dd3c65dcf418ebd02af0cfc2a4acfad2c5cd8b49
Akanksha Verma <22906.akanksha@gmail.com>
2021-09-04 20:30:41 +0530

    Square of a number.

---
+++ Problem14.py
@@ -0,0 +1,11 @@
+# print the square of first 10 numbers
+i = 1
+while (i<=10):
+    print(f"Square of {i} is :" ,i**2)
+    i+=1
+    
+# print the square of first 10 numbers
+x = 1
+while (x<=10):
+    print(f"Square of {x} is :" ,x*x)
+    x+=1
